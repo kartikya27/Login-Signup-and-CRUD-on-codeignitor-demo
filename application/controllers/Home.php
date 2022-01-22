40731a36865df7b2988e91e532ff0b7a73cf5776
Kartik Mandothiya <kartikya27@gmail.com>
2022-01-22 20:36:52 +0530

    Form Design

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,9 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		// $this->load->model('CheckDB');
+		// return $this->CheckDB->create_table();
+	}
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->model('Checkdb');
+		$result = $this->Checkdb->createTableSchema();
+		//echo $result;
+		$this->load->view('index');
+	}
+
+	public function login(){
+		$data = $this->input->post();
+		$this->load->library('form_validation');
+	    $this->form_validation->set_message('is_unique', 'This details are already exist, Please try another.');
+		$this->form_validation->set_rules('Email','Email','required|valid_email|is_unique[agents.Email]');
+		// print_r($data);
+
 	}
 }
---
+++ application/models/Checkdb.php
@@ -0,0 +1,131 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Checkdb extends CI_Model {
+
+    public function __construct() 
+            { }
+
+        public function createTableSchema()
+            {
+                $result = 0;
+                 $result += $this->_createAgentTableSchema();
+                 $result += $this->_createInsuranceTableSchema();
+                 return $result;
+
+            }
+
+        private function _createAgentTableSchema()
+        {
+            $this->load->dbforge();
+
+            $fields = array(
+                'id' => array(
+                    'type' => 'int',
+                    'constraint' => 211,
+                    'unsigned' => true,
+                    'auto_increment' => true
+                ),
+                'Email' =>array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'PhoneNum' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'Password' =>array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'Occuupation' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'AadharPanNumber' =>array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'AnnualIncome' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+               
+                'Address' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                
+            );
+
+            $this->dbforge->add_field($fields);
+            $this->dbforge->add_key('id',true);
+
+            return $this->dbforge->create_table('agents',true);
+        }
+
+    
+        private function _createInsuranceTableSchema()
+        {
+            $this->load->dbforge();
+
+            $fields = array(
+                'id' => array(
+                    'type' => 'int',
+                    'constraint' => 211,
+                    'unsigned' => true,
+                    'auto_increment' => true
+                ),
+                'insuranceID' =>array(
+                    'type' => 'int',
+                    'constraint' => 11,
+                ),
+                'insuranceAmount' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'insuranceType' =>array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'agentID' => array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                ),
+                'duration' =>array(
+                    'type' => 'varchar',
+                    'constraint' => 211,
+                    'NULL' => true
+                )
+                
+                
+            );
+
+            $this->dbforge->add_field($fields);
+            $this->dbforge->add_key('id',true);
+
+            return $this->dbforge->create_table('insurance',true);
+        }
+
+
+
+
+
+
+
+}
+
+
+
+
+
